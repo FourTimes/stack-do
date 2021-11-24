b406f829c5caf4c39082ada201b4e26d7fabd5cb
fourtimes <Jino.J@gds.ey.com>
2021-11-24 10:05:57 +0530

    prod and non-prod code modified

--- non-prod/provider.tf
+++ non-prod/provider.tf
@@ -2,12 +2,5 @@ provider "aws" {
   region = "ap-southeast-1"
 }
 
-terraform {
-  backend "s3" {
-    bucket = "sa-terraform-backend-state-store"
-    key    = "non-prod/terraform.tfstate"
-    region = "ap-southeast-1"
-  }
-}
 
 
--- prod/provider.tf
+++ prod/provider.tf
@@ -2,12 +2,4 @@ provider "aws" {
   region = "ap-southeast-1"
 }
 
-terraform {
-  backend "s3" {
-    bucket = "sa-terraform-backend-state-store"
-    key    = "non-prod/terraform.tfstate"
-    region = "ap-southeast-1"
-  }
-}
-
 
